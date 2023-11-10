e44aebae2bcfd70ced9b741a6be2f03d021ecfe7
Ali Alaql <23135660+alikhleel@users.noreply.github.com>
2023-11-10 13:02:44 +0300

    fix not found module

--- my-app/src/app/components/SubscribeForm.tsx
+++ my-app/src/app/components/SubscribeForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { submitSubscribeForm } from "@/app/actions";
 import { useRouter } from "next/navigation";
 
 export default function SubscribeForm() {
